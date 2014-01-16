7c7dc1d37216a8ae7596423f3d589880758f276a
Adam <leblan7@gmail.com>
2014-01-15 23:02:16 -0800

    quick save

--- assignments/session02/http_server.py
+++ assignments/session02/http_server.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
+
 import socket
 import sys
+from os import path
+import os
+import mimetypes
 
 
-def response_ok():
+def response_ok(body="", mimetype='text/plain'):
     """returns a basic HTTP response"""
     resp = []
     resp.append("HTTP/1.1 200 OK")
-    resp.append("Content-Type: text/plain")
+    resp.append("Content-Type: " + mimetype)
     resp.append("")
-    resp.append("this is a pretty minimal response")
+    resp.append(body)
     return "\r\n".join(resp)
 
 
@@ -19,6 +24,12 @@ def response_method_not_allowed():
     resp.append("")
     return "\r\n".join(resp)
 
+def response_not_found():
+    """returns a 404 response"""
+    resp = []
+    resp.append("HTTP/1.1 404 Not Found")
+    resp.append("")
+    return "\r\n".join(resp)
 
 def parse_request(request):
     first_line = request.split("\r\n", 1)[0]
@@ -26,7 +37,18 @@ def parse_request(request):
     if method != "GET":
         raise NotImplementedError("We only accept GET")
     print >>sys.stderr, 'request is okay'
+    return uri
 
+def resolve_uri(uri):
+    fullPath = 'webroot/%s' % uri
+    print fullPath
+    if not path.exists(fullPath):
+        raise IOError
+    if path.isdir(fullPath):
+        return ['text/plain','\r\n'.join(os.listdir(fullPath))]
+    elif path.isfile(fullPath):
+        mType = mimetypes.guess_type(fullPath)
+        return [mType[0],open(fullPath,'rb').read()]
 
 def server():
     address = ('127.0.0.1', 10000)
@@ -50,9 +72,13 @@ def server():
                         break
 
                 try:
-                    parse_request(request)
+                    uri = parse_request(request)
+                    mType, body = resolve_uri(uri)
+                    response = response_ok(body,mType)
                 except NotImplementedError:
                     response = response_method_not_allowed()
+                except IOError:
+                    response = response_not_found()
                 else:
                     response = response_ok()
 
